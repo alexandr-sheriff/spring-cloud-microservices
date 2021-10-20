9390808d9e2959894b8ebb3527dde21f41da0d1d
Alexandr Shafarenko <alexandr.shafarenko@gmail.com>
2021-10-21 01:24:02 +0300

    Add Unit and Integration tests for Deposit-Service

--- deposit-service/src/main/java/com/alex/deposit/rest/account/AccountResponseDTO.java
+++ deposit-service/src/main/java/com/alex/deposit/rest/account/AccountResponseDTO.java
@@ -3,11 +3,13 @@ package com.alex.deposit.rest.account;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountResponseDTO {
--- deposit-service/src/main/java/com/alex/deposit/rest/bill/BillResponseDTO.java
+++ deposit-service/src/main/java/com/alex/deposit/rest/bill/BillResponseDTO.java
@@ -3,11 +3,13 @@ package com.alex.deposit.rest.bill;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BillResponseDTO {
